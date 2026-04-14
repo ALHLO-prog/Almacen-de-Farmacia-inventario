57d08991663f57d6fa2bb14dbe0489ce489ff5bd
Andrés Rodríguez <andres.rodriguezv05@gmail.com>
2026-04-14 18:40:54 -0400

    Creacion de ruta para logueo del usuario

--- backend/routes/usuarios.js
+++ backend/routes/usuarios.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import db from '../dbconnection.js'
+import bcrypt from 'bcrypt'
 
 const router = express.Router()
 
@@ -10,6 +11,33 @@ router.get('/:ci', async (req, res) => {
     res.json(usuarios)
   } catch (e) {
     res.status(500).json({ mensaje: 'Error al obtener usuario' })
+  }
+})
+
+router.post('/login', async (req, res) => {
+  const { ci, contraseña } = req.body
+
+  try {
+    const usuario = await db('usuarios').where({ ci }).first()
+    if (!usuario) {
+      return res.status(404).json({ mensaje: 'Usuario no encontrado' })
+    }
+
+    const passwordCorrecto = await bcrypt.compare(
+      contraseña,
+      usuario.contraseña
+    )
+    if (passwordCorrecto) {
+      const { contraseña: _, ...datosUsuario } = usuario
+      res.json({
+        usuario: datosUsuario
+      })
+    } else {
+      res.status(401).json({ mensaje: 'Contraseña incorrecta' })
+    }
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ mensaje: 'Error interno del servidor' })
   }
 })
 
