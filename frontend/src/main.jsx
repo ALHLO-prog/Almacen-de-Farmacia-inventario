254d05c0e6235ab7f49692f36b69667ebb4be078
Andrés Rodríguez <andres.rodriguezv05@gmail.com>
2026-04-24 18:17:13 -0400

    queryClient configuracion

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,13 +5,25 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { LocalizationProvider } from '@mui/x-date-pickers'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 1, // Los datos se consideran "frescos" por 1 minuto
+      refetchOnWindowFocus: false // Evita recargar cada vez que cambias de pestaña
+    }
+  }
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <App />
-      </LocalizationProvider>
+      <QueryClientProvider client={queryClient}>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <App />
+        </LocalizationProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
 )
